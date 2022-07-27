6467b0c7353c0e63ebf01b68fd63637b563b3393
yaswanthsaireddy <53967143+yaswanthsaireddy@users.noreply.github.com>
2022-07-27 16:33:07 +0530

    Update settings.py

--- devvsearch/settings.py
+++ devvsearch/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@5$1f46-831ng(3=kfi50a6a2zvycj6^%67fr3vj^1p59kdj)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
